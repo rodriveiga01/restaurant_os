454ee1cf57b9a2149b599d15a3bcdb20327d846e
rodriveiga01 <rodriveiga01@gmail.com>
2024-09-06 15:28:52 +0100

    Fixed issues

--- app/utils/supabase/server.ts
+++ app/utils/supabase/server.ts
@@ -4,13 +4,22 @@ import { cookies } from 'next/headers'
 export function createClient() {
     const cookieStore = cookies()
 
+    const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+    const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+    if (!supabaseUrl || !supabaseKey) {
+        throw new Error('Supabase URL or Anon Key is not defined');
+    }
+
     return createServerClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        supabaseUrl,
+        supabaseKey,
         {
-            cookies: {
-                get(name: string) {
-                    return cookieStore.get(name)?.value
+            cookies: cookieStore,
+            cookieOptions: {
+                getAll: () => cookieStore.getAll(),
+                setAll: (cookiesToSet: { name: any; value: any; options: any }[]) => {
+                    cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options))
                 },
             },
         }
